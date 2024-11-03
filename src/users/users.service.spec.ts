663c371f6956b883925439ee0207254b0316ccbd
kkozhikhov <kkozhikhov@gmail.com>
2024-11-03 10:11:04 +0200

    add unit tests users service

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,84 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { CreateUserDto } from 'src/auth/dtos/create-user.dto';
+import { DRIZZLE } from 'src/drizzle/drizzle.module';
+import { users } from 'src/drizzle/schema';
 
 describe('UsersService', () => {
-  let service: UsersService;
+  let usersService: UsersService;
+
+  const user = {
+    id: 1,
+    username: 'jdoe',
+    password: 'hashedPassword',
+    firstName: 'John',
+    lastName: 'Doe',
+  };
+
+  const mockDrizzleDB = {
+    query: {
+      users: {
+        findFirst: jest.fn() as jest.Mock,
+      },
+    },
+    insert: jest.fn().mockReturnThis(),
+    values: jest.fn().mockReturnThis(),
+    returning: jest.fn().mockResolvedValue([user]),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: DRIZZLE,
+          useValue: mockDrizzleDB,
+        },
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    usersService = module.get<UsersService>(UsersService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('findByUsername', () => {
+    it('should return a user when found', async () => {
+      mockDrizzleDB.query.users.findFirst.mockResolvedValue(user);
+
+      const result = await usersService.findByUsername('jdoe');
+
+      expect(mockDrizzleDB.query.users.findFirst).toHaveBeenCalledWith({
+        where: expect.any(Function),
+      });
+
+      expect(result).toEqual(user);
+    });
+
+    it('should return null when user is not found', async () => {
+      mockDrizzleDB.query.users.findFirst.mockResolvedValue(null);
+
+      const result = await usersService.findByUsername('unknown');
+
+      expect(mockDrizzleDB.query.users.findFirst).toHaveBeenCalled();
+      expect(result).toBeNull();
+    });
+  });
+
+  describe('create', () => {
+    it('should create a user and return it', async () => {
+      const createUserDto: CreateUserDto = {
+        username: 'jdoe',
+        password: 'password',
+        firstName: 'John',
+        lastName: 'Doe',
+      };
+
+      const result = await usersService.create(createUserDto);
+
+      expect(result).toEqual([user]);
+
+      expect(mockDrizzleDB.insert).toHaveBeenCalledWith(users);
+      expect(mockDrizzleDB.values).toHaveBeenCalledWith(createUserDto);
+      expect(mockDrizzleDB.returning).toHaveBeenCalled();
+    });
   });
 });
